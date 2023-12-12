46c88155ea2284ab28f82a06988a78c8c4482eb6
Siwei Liu <36103884+zsulsw@users.noreply.github.com>
2023-12-12 12:01:03 +0800

    Update BeamColumn.py

--- Source/NeuralNetwork/BeamColumn.py
+++ Source/NeuralNetwork/BeamColumn.py
@@ -1,3 +1,9 @@
+###########################################################################################
+# Machine Learning-based Second-order Analysis of Beam-columns through PINNs
+# Developed by Siwei Liu, Liang Chen and Haoyi Zhang
+# License: GPL-3.0
+###########################################################################################
+
 import os
 import torch
 import torch.nn as nn
